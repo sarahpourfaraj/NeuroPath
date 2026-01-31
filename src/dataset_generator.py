bff40effc2f75974d89b98a164ff66824c5238af
sarah <sarahpourfaraj@gmail.com>
2026-01-31 23:32:58 +0330

    updated dataset generator and edited path tracer to be better & have depth too

---
+++ src/dataset_generator.py
@@ -0,0 +1,46 @@
+import os
+import numpy as np
+import imageio
+
+from path_tracer import random_scene, render_with_scene
+
+NOISY_AOVS = ["color", "albedo", "normal", "depth"]
+NUM_IMAGES = 50
+
+for aov in NOISY_AOVS:
+    os.makedirs(f"../data/noisy/{aov}", exist_ok=True)
+
+os.makedirs("../data/clean/color", exist_ok=True)
+
+for i in range(NUM_IMAGES):
+    print(f"Rendering scene {i}")
+    np.random.seed(i)
+
+    spheres, env_light = random_scene()
+
+    # noisy: all AOVs
+    for aov in NOISY_AOVS:
+        noisy = render_with_scene(
+            spheres, env_light,
+            samples_per_pixel=1,
+            max_depth=3,
+            aov=aov
+        )
+
+        imageio.imwrite(
+            f"../data/noisy/{aov}/img_{i}.png",
+            (noisy * 255).astype(np.uint8)
+        )
+
+    # clean: jsut color
+    clean = render_with_scene(
+        spheres, env_light,
+        samples_per_pixel=128,
+        max_depth=3,
+        aov="color"
+    )
+
+    imageio.imwrite(
+        f"../data/clean/color/img_{i}.png",
+        (clean * 255).astype(np.uint8)
+    )
