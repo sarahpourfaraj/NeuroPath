5045635b18c921961179a93d01ec95e194f91bc8
sarahpourfaraj <159299851+sarahpourfaraj@users.noreply.github.com>
2026-02-07 16:34:47 +0330

    updated dataseet generator from 50 to 600

--- src/rendering/dataset_generator.py
+++ src/rendering/dataset_generator.py
@@ -2,10 +2,11 @@ import os
 import numpy as np
 import imageio
 
-from path_tracer import random_scene, render_with_scene
+from path_tracer import render_with_scene
+from scenes import random_scene
 
 NOISY_AOVS = ["color", "albedo", "normal", "depth"]
-NUM_IMAGES = 50
+NUM_IMAGES = 480 # simple + mixed only
 
 for aov in NOISY_AOVS:
     os.makedirs(f"../data/noisy/{aov}", exist_ok=True)
@@ -14,9 +15,7 @@ os.makedirs("../data/clean/color", exist_ok=True)
 
 for i in range(NUM_IMAGES):
     print(f"Rendering scene {i}")
-    np.random.seed(i)
-
-    spheres, env_light = random_scene()
+    spheres, env_light = random_scene(i)
 
     # noisy: all AOVs
     for aov in NOISY_AOVS:
